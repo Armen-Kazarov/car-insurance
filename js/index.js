12c6573e00d884899bdb46d57faccc0c21f52f45
Armen-Kazarov <jonybravocn.jb@gmail.com>
2025-01-25 01:42:44 +0100

    created and finished popup, created and finished form in popup, implemented sending form data, created temp-pages

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 "use strict"
-
+document.addEventListener("DOMContentLoaded", () => {
   function burgerMenu() {
     const siteLogo = document.querySelector('.site-logo');
     const iconMenuOpen = document.querySelector('.menu-icon-open');
@@ -39,3 +39,50 @@
   }
   
   burgerMenu();
+
+  //popup
+  const popupForm = {
+    modal: document.querySelector('.js-lightbox'),
+    lightboxForm: document.querySelector('.popup-form'),
+    btnOpenModal: document.querySelector('.request-call-btn'),
+    btn: document.querySelector('.popup-button')
+  };
+
+  const showModal = (event) => {
+    event.preventDefault();
+    const { target } = event;
+    popupForm.modal.classList.add('is-open');
+  };
+
+  const hideModal = (event) => {
+    const { target } = event;
+
+    if(target.nodeName === 'IMG' || target.nodeName === 'DIV') {
+      popupForm.modal.classList.remove('is-open');
+    }
+  };
+
+  const hideModalWithEsc = (event) => {
+
+    if(event.code === 'Escape') {
+      popupForm.modal.classList.remove('is-open');
+    }
+  };
+
+  popupForm.btnOpenModal.addEventListener('click', showModal);
+  popupForm.modal.addEventListener('click', hideModal);
+  document.addEventListener('keydown', hideModalWithEsc);
+
+  //form submit
+  const registrationForm = document.querySelector('.contacts-section-form');
+  
+  registrationForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const regFormData = new FormData(event.target);
+    const data = Object.fromEntries(regFormData.entries());
+    console.log(data);
+
+    window.location.href = '../temp-pages/tempPage1.html';
+  });
+})
